f32c4909e4ee1ba2207b4603f8cf716e02b2f4fb
Kayla-Jasmine <kimbrah01@wildcats.jwu.edu>
2019-10-09 12:45:33 -0400

    IMAGES
    Top broken heart image wont show up

--- js/app.js
+++ js/app.js
@@ -3,14 +3,20 @@ $(document).foundation()
 var slideIndex = 0;
 showSlides();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+$('.ul a').click(function(event){
+  
+  // prevent defalt click behavior
+  // don't jump to content
+  event.preventDefault();
+    
+  // identify position of target
+  var target = $(this).attr('href');
+  var top = $(target).offset().top;
+
+  // animate scroll to target
+  // set speed to 700ms
+  $('html,body').animate({
+    scrollTop: top
+    },700);
+
+});
